b553a797d56186316ca0136d6cc0ba8d9111af74
spodsvirov <spodsvirov@tradingview.com>
2023-04-27 21:56:41 +0400

    3.3.8: get history usecase

---
+++ internal/usecases/client/get-history/dto.go
@@ -0,0 +1,75 @@
+package gethistory
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	messagesrepo "github.com/karasunokami/chat-service/internal/repositories/messages"
+	"github.com/karasunokami/chat-service/internal/types"
+	"github.com/karasunokami/chat-service/internal/validator"
+)
+
+type Request struct {
+	ID       types.RequestID `validate:"required"`
+	ClientID types.UserID    `validate:"required"`
+	PageSize int             `validate:"omitempty,gte=10,lte=100"`
+	Cursor   string          `validate:"omitempty,base64url"`
+}
+
+func (r Request) Validate() error {
+	if r.PageSize == 0 && r.Cursor == "" {
+		return errors.New("page size or cursor must be provided")
+	}
+
+	if r.PageSize != 0 && r.Cursor != "" {
+		return errors.New("page size or cursor must be provided")
+	}
+
+	if err := r.ID.Validate(); err != nil {
+		return fmt.Errorf("request id is invalid, err=%v", err)
+	}
+
+	if err := r.ClientID.Validate(); err != nil {
+		return fmt.Errorf("client id is invalid, err=%v", err)
+	}
+
+	return validator.Validator.Struct(r)
+}
+
+type Response struct {
+	NextCursor string
+	Messages   []Message
+}
+
+type Message struct {
+	ID         types.MessageID
+	AuthorID   types.UserID
+	Body       string
+	CreatedAt  time.Time
+	IsReceived bool
+	IsBlocked  bool
+	IsService  bool
+}
+
+func adoptMessages(messages []messagesrepo.Message) []Message {
+	msgs := make([]Message, 0, len(messages))
+
+	for _, message := range messages {
+		msgs = append(msgs, adoptMessage(message))
+	}
+
+	return msgs
+}
+
+func adoptMessage(m messagesrepo.Message) Message {
+	return Message{
+		ID:         m.ID,
+		AuthorID:   m.AuthorID,
+		Body:       m.Body,
+		CreatedAt:  m.CreatedAt,
+		IsReceived: m.IsVisibleForManager && !m.IsBlocked,
+		IsBlocked:  m.IsBlocked,
+		IsService:  m.IsService,
+	}
+}
---
+++ internal/usecases/client/get-history/usecase.go
@@ -0,0 +1,104 @@
+package gethistory
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/karasunokami/chat-service/internal/cursor"
+	messagesrepo "github.com/karasunokami/chat-service/internal/repositories/messages"
+	"github.com/karasunokami/chat-service/internal/types"
+)
+
+//go:generate mockgen -source=$GOFILE -destination=mocks/usecase_mock.gen.go -package=gethistorymocks
+
+var (
+	ErrInvalidRequest = errors.New("invalid request")
+	ErrInvalidCursor  = errors.New("invalid cursor")
+)
+
+type messagesRepository interface {
+	GetClientChatMessages(
+		ctx context.Context,
+		clientID types.UserID,
+		pageSize int,
+		cursor *messagesrepo.Cursor,
+	) ([]messagesrepo.Message, *messagesrepo.Cursor, error)
+}
+
+//go:generate options-gen -out-filename=usecase_options.gen.go -from-struct=Options
+type Options struct {
+	msgRepo messagesRepository `option:"mandatory" validate:"required"`
+}
+
+type UseCase struct {
+	Options
+}
+
+func New(opts Options) (UseCase, error) {
+	err := opts.Validate()
+	if err != nil {
+		return UseCase{}, fmt.Errorf("validate options, err=%v", err)
+	}
+
+	return UseCase{opts}, nil
+}
+
+func (u UseCase) Handle(ctx context.Context, req Request) (Response, error) {
+	err := req.Validate()
+	if err != nil {
+		return Response{}, fmt.Errorf("request validate, err=%w", ErrInvalidRequest)
+	}
+
+	crs, err := decodeCursor(req.Cursor)
+	if err != nil {
+		if err != errEmptyCursor {
+			return Response{}, fmt.Errorf("codecode cursor, err=%w", err)
+		}
+	}
+
+	msgs, nextCrs, err := u.msgRepo.GetClientChatMessages(ctx, req.ClientID, req.PageSize, crs)
+	if err != nil {
+		if errors.Is(err, messagesrepo.ErrInvalidCursor) {
+			return Response{}, fmt.Errorf("get client chat messages, err=%w, err=%v", ErrInvalidCursor, err)
+		}
+
+		return Response{}, fmt.Errorf("get client chat messages, err=%w", err)
+	}
+
+	return formatResp(msgs, nextCrs)
+}
+
+func formatResp(msgs []messagesrepo.Message, nextCrs *messagesrepo.Cursor) (Response, error) {
+	var err error
+
+	resp := Response{
+		Messages: adoptMessages(msgs),
+	}
+
+	if nextCrs != nil {
+		resp.NextCursor, err = cursor.Encode(nextCrs)
+		if err != nil {
+			return Response{}, fmt.Errorf("encode cursor, err=%v", err)
+		}
+	}
+
+	return resp, nil
+}
+
+var errEmptyCursor = errors.New("empty cursor")
+
+func decodeCursor(cursorString string) (*messagesrepo.Cursor, error) {
+	if cursorString == "" {
+		return nil, errEmptyCursor
+	}
+
+	crs := &messagesrepo.Cursor{}
+
+	err := cursor.Decode(cursorString, crs)
+	if err != nil {
+		return nil, fmt.Errorf("decode cursor, err=%w", ErrInvalidCursor)
+	}
+
+	return crs, nil
+}
